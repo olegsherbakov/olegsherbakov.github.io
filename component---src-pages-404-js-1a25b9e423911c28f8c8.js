c42361ac509d4a44e2fe6e645fcde0b507054708
olegsherbakov <sherbakov.olegik@gmail.com>
2020-07-26 16:11:01 +0300

    Updates

---
+++ component---src-pages-404-js-1a25b9e423911c28f8c8.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),l=a.n(n),s=a("Bl7J");t.default=function(){return l.a.createElement(s.a,null,l.a.createElement("div",null,l.a.createElement("h1",null,l.a.createElement("span",{"aria-labelledby":"jsx-a11y/accessible-emoji",role:"img"},"🤔")),l.a.createElement("h3",null,"You just hit a route that doesn't exist... the sadness.")))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-1a25b9e423911c28f8c8.js.map
--- sw.js
+++ sw.js
@@ -1,109 +1,13 @@
-/**
- * Welcome to your Workbox-powered service worker!
- *
- * You'll need to register this file in your web app and you should
- * disable HTTP caching for this file too.
- * See https://goo.gl/nhQhGp
- *
- * The rest of the code is auto-generated. Please don't update this file
- * directly; instead, make changes to your Workbox build configuration
- * and re-run your build process.
- * See https://goo.gl/2aRDsh
- */
+self.addEventListener('install', function(e) {
+  self.skipWaiting();
+});
 
-importScripts("workbox-v3.6.1/workbox-sw.js");
-workbox.setConfig({modulePathPrefix: "workbox-v3.6.1"});
-
-workbox.core.setCacheNameDetails({prefix: "gatsby-plugin-offline"});
-
-workbox.skipWaiting();
-workbox.clientsClaim();
-
-/**
- * The workboxSW.precacheAndRoute() method efficiently caches and responds to
- * requests for URLs in the manifest.
- * See https://goo.gl/S9QRab
- */
-self.__precacheManifest = [
-  {
-    "url": "webpack-runtime-78e4936cc0305d539a68.js"
-  },
-  {
-    "url": "commons.8dbe43c0455fd98ae87a.css"
-  },
-  {
-    "url": "commons-5027c06f7a95952e98b5.js"
-  },
-  {
-    "url": "app-762774afbd0e7852e0ac.js"
-  },
-  {
-    "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-2b3ad3761a0f7f950eb9.js"
-  },
-  {
-    "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "dbf441ca2ea017f11d9ce2b1e0c8b0b1"
-  },
-  {
-    "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
-    "revision": "1268873dbbdb50b655e0f060a65503da"
-  },
-  {
-    "url": "manifest.webmanifest",
-    "revision": "b9b8598b4f05a135101e9b12a4a4f877"
-  }
-].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
-
-workbox.routing.registerRoute(/(\.js$|\.css$|static\/)/, workbox.strategies.cacheFirst(), 'GET');
-workbox.routing.registerRoute(/^https?:.*\page-data\/.*\/page-data\.json/, workbox.strategies.networkFirst(), 'GET');
-workbox.routing.registerRoute(/^https?:.*\.(png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/, workbox.strategies.staleWhileRevalidate(), 'GET');
-workbox.routing.registerRoute(/^https?:\/\/fonts\.googleapis\.com\/css/, workbox.strategies.staleWhileRevalidate(), 'GET');
-
-/* global importScripts, workbox, idbKeyval */
-
-importScripts(`idb-keyval-iife.min.js`)
-
-const { NavigationRoute } = workbox.routing
-
-const navigationRoute = new NavigationRoute(async ({ event }) => {
-  let { pathname } = new URL(event.request.url)
-  pathname = pathname.replace(new RegExp(`^`), ``)
-
-  // Check for resources + the app bundle
-  // The latter may not exist if the SW is updating to a new version
-  const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-762774afbd0e7852e0ac.js`))) {
-    return await fetch(event.request)
-  }
-
-  for (const resource of resources) {
-    // As soon as we detect a failed resource, fetch the entire page from
-    // network - that way we won't risk being in an inconsistent state with
-    // some parts of the page failing.
-    if (!(await caches.match(resource))) {
-      return await fetch(event.request)
-    }
-  }
-
-  const offlineShell = `/offline-plugin-app-shell-fallback/index.html`
-  return await caches.match(offlineShell)
-})
-
-workbox.routing.registerRoute(navigationRoute)
-
-const messageApi = {
-  setPathResources(event, { path, resources }) {
-    event.waitUntil(idbKeyval.set(`resources:${path}`, resources))
-  },
-
-  clearPathResources(event) {
-    event.waitUntil(idbKeyval.clear())
-  },
-}
-
-self.addEventListener(`message`, event => {
-  const { gatsbyApi } = event.data
-  if (gatsbyApi) messageApi[gatsbyApi](event, event.data)
-})
+self.addEventListener('activate', function(e) {
+  self.registration.unregister()
+    .then(function() {
+      return self.clients.matchAll();
+    })
+    .then(function(clients) {
+      clients.forEach(client => client.navigate(client.url))
+    });
+});
